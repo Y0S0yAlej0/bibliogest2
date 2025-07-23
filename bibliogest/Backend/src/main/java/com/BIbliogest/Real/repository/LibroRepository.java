bd11d270b03e3b7acdd169e951855ca08904c383
Y0S0yAlej0 <167207369+Y0S0yAlej0@users.noreply.github.com>
2025-07-23 15:33:34 -0500

    Delete bibliogest/Backend directory

--- bibliogest/Backend/src/main/java/com/BIbliogest/Real/repository/LibroRepository.java
+++
@@ -1,7 +0,0 @@
-package com.BIbliogest.Real.repository;
-
-import com.BIbliogest.Real.model.Libro;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LibroRepository extends JpaRepository<Libro, Long> {
-}
--- bibliogest/Backend/src/main/java/com/BIbliogest/Real/repository/ResenaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.BIbliogest.Real.repository;
-
-import com.BIbliogest.Real.model.Resena;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ResenaRepository extends JpaRepository<Resena, Long> {
-}
